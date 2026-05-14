3ab0cc60d0cfa019881e3caf98de281ea1278173
Chakradhar Reddy Yerragudi <chakradharvasurama@gmail.com>
2026-05-14 23:42:43 +0530

    Add: count-connected-components - submission-4

---
+++ Data Structures & Algorithms/count-connected-components/submission-4.py
@@ -0,0 +1,31 @@
+class Solution:
+    def countComponents(self, n: int, edges: List[List[int]]) -> int:
+
+        hmap=defaultdict(list)
+      
+
+        for edge in edges:
+            hmap[edge[0]].append(edge[1])
+            hmap[edge[1]].append(edge[0])
+        visited=[0]*n
+        def dfs(x:int):
+            visited[x]=1
+            for i in hmap[x]:
+                if not visited[i]:
+                    dfs(i)
+        ans=0
+        for i in range(n):
+            if not visited[i]:
+                dfs(i)
+                ans+=1
+        return ans
+
+
+        
+
+
+        
+        
+
+            
+        
